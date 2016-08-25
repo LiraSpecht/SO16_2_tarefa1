6cdb10cf35a0fb2219488d1a6e67cd4104495192
LiraSpecht <lira_specht@hotmail.com>
2016-08-25 14:51:13 -0300

    pthreadWin32
    Tentando utilizar o pthread para win32, ainda com problemas de
    compatibilidade, nao reconhece chamadas de funcao

--- simpletest/examples/calculator/test.c
+++ simpletest/examples/calculator/test.c
@@ -1,5 +1,7 @@
 #include "../../simpletest.h"
 #include "calculator.h"
+// Inlcuindo pthread no Visual Studio: https://linqtolinq.wordpress.com/2012/04/24/setting-up-pthreads-in-windows-under-visual-studio/
+#include "../trab1SO/pthread-win32/pthread.h"
 
 void test_sum(){
 
@@ -23,16 +25,50 @@ void test_sub() {
 	WHEN("I execute sub");
 	IF("use numbers 3 and 5");
 	THEN("the result should be -2");
-	isEqual(sub(3,5), -2);
+	isEqual(sub(1,5), -2);
 
 	IF("use numbers 0 and 0");
 	THEN("the result should be 0");
 	isEqual(sum(0, 0), 0);
 }
 
-int main () {
-	test_sum();
-	test_sub();
-	system("PAUSE");
+void *etapa3(void *arg) {
+	int x = 0;
+	DESCRIBE("SOMA X");
+	WHEN("I execute sum");
+	IF("use numbers 0 and 1");
+	THEN("the result should be 1");
+	isEqual(sum(x, 1), 1);
+	return NULL;
 }
 
+/*void doTheParallel() {
+	pthread_t threads;
+	pthread_create(&(threads), NULL, etapa3, NULL);
+	pthread_join(threads, NULL);
+}*/
+
+void *thread_func(void *arg) {
+	return 0;
+}
+
+
+
+int main () {
+	//test_sum();
+	//test_sub();
+
+	//1. Clonar o Simpletest de https://github.com/bundz/simpletest : DONE, firstCommit
+	//2. Olhar o exemplo de calculadora : DONE, firstCommit
+	//3. Criar uma função em C que calcule o número de combinações possíveis de resultados de n threads executando x = x + 1 numa variável x compartilhada e não protegida por exclusão mútua. 
+	//4. Criar casos de teste para a função usando simpletest. 
+	//5. Submeter um único arquivo de texto contendo o código com a função e os casos de teste.
+
+	//paralelismo();
+
+	pthread_t threads;
+	pthread_create(&threads, NULL, thread_func, NULL);
+	pthread_join(threads, NULL);
+
+	system("PAUSE");
+}
