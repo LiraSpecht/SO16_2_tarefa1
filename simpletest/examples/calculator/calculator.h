58f6df0df4b72233b857bcee3f6293bbf30fbf5f
LiraSpecht <lira_specht@hotmail.com>
2016-08-25 10:32:18 -0300

    firstCommit
    Projeto clonado e incluido junto ao Visual Studio, execucao do test
    realizado com sucesso

---
+++ simpletest/examples/calculator/calculator.h
@@ -0,0 +1,10 @@
+#ifndef CALCULATOR_H
+#define CALCULATOR_H
+
+
+int sum(int, int);
+int sub(int, int);
+int mult(int, int);
+int divi(int, int); //div deu conflito com uma função do stdlib.h, chamado nativamente no visual studio
+
+#endif
