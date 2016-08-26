570fa75df5b9d738882ec5a5fb74871437bae32b
LiraSpecht <lira_specht@hotmail.com>
2016-08-26 16:29:01 -0300

    threadResultsCounter
    Foi otimizado alguns codigos intermediarios e incluido a funcao que
    realiza a contagem de resultados obtidos

--- simpletest/examples/calculator/test.c
+++ simpletest/examples/calculator/test.c
@@ -4,12 +4,12 @@
 #define HAVE_STRUCT_TIMESPEC //REMOVE O ERRO --> "pthread.h(320): error C2011: 'timespec': 'struct' type redefinition"
 #include <pthread.h>
 
-#define MAX_CASES 3
+#define NUM_THREADS 8 //Define número de threads
+#define CASOSTESTES 100 //Define a quantidade de casos de testes serão executados
 
-int *GLOBALVAR;
+int GLOBALVAR = 0; //variável compartilhada
 
 void test_sum(){
-
 	DESCRIBE("SUM");
 	WHEN("I execute sum");
 	IF("use numbers 3 and 5");
@@ -43,66 +43,73 @@ char* concat(char *txt, int num) {
 	return buffer;
 }
 
-void *incGlobal(void *arg) {
+void *plusOne(void *arg) {
 	//Função paralela que incrementa um valor na variável global
-	int x = *GLOBALVAR; //Obtem o valor atual da variavel
-	int y = x + 1; //Calcula a soma que deveria ser obtido
-
-	char iftxt[255];
-	char thentxt[255];
-
-	sprintf(iftxt, "use numbers %ld and 1", x);
-	sprintf(thentxt, "the result should be %ld", y);
 
 	DESCRIBE("SOMA GLOBAL");
 	WHEN("I execute sum");
+
+	int valGlobal = GLOBALVAR;
+	int result = valGlobal + 1;
+
+	char iftxt[50];
+	char thentxt[50];
+	sprintf(iftxt, "use numbers %ld and 1", GLOBALVAR);
+	sprintf(thentxt, "the result should be %ld", result);
+	
 	IF(iftxt);
 	THEN(thentxt);
-	*GLOBALVAR = *GLOBALVAR + 1; //Realiza a soma de verdade
-	x = *GLOBALVAR;
-	isEqual(sum(x, 1), y); //Verifica se a soma surtiu efeito 
-}
 
-void *decGlobal(void *arg) {
-	//Função paralela que decrementa um valor na variável global
-	int x = *GLOBALVAR; //Obtem o valor atual da variavel
-	int y = x - 1; //calcula a subtração que deveria ser obtida
-	char iftxt[255];
-	char thentxt[255];
-
-	sprintf(iftxt, "use numbers %ld and 1", x);
-	sprintf(thentxt, "the result should be %ld", y);
-
-	DESCRIBE("SUB GLOBAL");
-	WHEN("I execute sub");
-	IF(iftxt);
-	THEN(thentxt);
-	*GLOBALVAR = *GLOBALVAR - 1; //Realiza a subtração de verdade
-	x = *GLOBALVAR;
-	isEqual(sub(x, 1), y); //Verifica se a subtração surtiu efeito
-}
-
-void *contaCasos(void *arg) {
-	//Função que conta a quantidade de possibilidades de resultados
+	isEqual(sum(GLOBALVAR, 1), result); //Verifica se a soma surtiu efeito 
+	GLOBALVAR = result; //Atribui a variavel compartilhada o valor da soma no inicio do processo
 }
 
 void doTheParallel() {
 	//Função que irá realizar o paralelismo
 	
-	pthread_t threads[MAX_CASES]; //Cria unidades de paralelismos
-	for (int i = 0; i < MAX_CASES; i++) {
+	pthread_t threads[NUM_THREADS]; //Cria unidades de paralelismos
+	for (int i = 0; i < NUM_THREADS; i++) {
 		//Cria threads e executa-as, foi separado em incremento e decremento para tornar "mais dinâmico"
-		if(i % 2 == 0)
-			pthread_create(&(threads[i]), NULL, incGlobal, NULL);
-		else
-			pthread_create(&(threads[i]), NULL, decGlobal, NULL);
+		pthread_create(&(threads[i]), NULL, plusOne, NULL);
 	}
-	for (int i = 0; i < MAX_CASES; i++) {
+	for (int i = 0; i < NUM_THREADS; i++) {
 		//Aguarda o fim de cada thread
 		pthread_join(threads[i], NULL);
 	}
 }
 
+void contaCasos() {
+	//Função que conta a quantidade de possibilidades de resultados
+	int contador[NUM_THREADS + 1]; //Teoricamente, há NUM_THREADS - 1 casos de resposta, de 1 à NUM_THREAD
+									//No pior caso, cada thread é executada uma após a outra, resultando em NUM_THREAD
+	for (int i = 0; i < NUM_THREADS + 1; i++) {
+		contador[i] = -1; //"zerando" valores
+	}
+	
+	for (int i = 0; i < CASOSTESTES; i++) {
+		// Executa o paralelismo várias vezes
+		GLOBALVAR = 0;
+		doTheParallel();
+		contador[GLOBALVAR]++;
+	}
+	
+	int totalRespostasDiferentes = 0;
+	for (int i = 0; i < NUM_THREADS + 1; i++) {
+		if (contador[i] != -1) {
+			totalRespostasDiferentes++; // para cada valor diferente encontrado
+		}
+	}
+
+	char iftxt[255];
+	char thentxt[255];
+	sprintf(iftxt, "used %ld cases of test", CASOSTESTES);
+	sprintf(thentxt, "it was possible obtained a tottal of %ld different answer", totalRespostasDiferentes);
+
+	DESCRIBE("COUNTER");
+	WHEN("I execute counter");
+	IF(iftxt);
+	THEN(thentxt);
+}
 
 int main () {
 	//test_sum();
@@ -114,12 +121,7 @@ int main () {
 	//4. Criar casos de teste para a função usando simpletest. 
 	//5. Submeter um único arquivo de texto contendo o código com a função e os casos de teste.
 
-	int oGlobal = 0;
-	GLOBALVAR = &oGlobal; // Setando o valor inicial da variável global
+	contaCasos();
 
-	//for(int i = 0; i < MAX_CASES; i++) //Repete o teste várias vezes, [OPCIONAL]
-		doTheParallel();
-
-	printf("Global iniciou com ZERO, terminou com %i\n", *GLOBALVAR); //Mostra o valor final obtido no processo
 	system("PAUSE"); //Permite que se veja a saida dos dados
 }
