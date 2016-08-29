5890b2d029fb1ae053faae05026b8c5db74393ec
LiraSpecht <lira_specht@hotmail.com>
2016-08-29 12:12:43 -0300

    TestsIncluded
    Foi adicionado alguns casos de testes, cada um sendo ativado atraves da
    modificacao do define TESTENUMERO

--- simpletest/examples/calculator/test.c
+++ simpletest/examples/calculator/test.c
@@ -1,13 +1,54 @@
+/****************************************************/
+/****************************************************/
+/*        Trabalho 1 de Sistemas Operacionais       */
+/*              Autoria de Lira Specht              */
+/*                 Semestre 2016-2                  */
+/****************************************************/
+/****************************************************/
+
+
 #include "../../simpletest.h"
 #include "calculator.h"
-// Inlcuindo pthread no Visual Studio: http://web.cs.du.edu/~sturtevant/w13-sys/InstallingpthreadsforVisualStudio.pdf
+
+// Incluindo pthread no Visual Studio: http://web.cs.du.edu/~sturtevant/w13-sys/InstallingpthreadsforVisualStudio.pdf
 #define HAVE_STRUCT_TIMESPEC //REMOVE O ERRO --> "pthread.h(320): error C2011: 'timespec': 'struct' type redefinition"
 #include <pthread.h>
 
-#define NUM_THREADS 8 //Define número de threads
-#define CASOSTESTES 100 //Define a quantidade de casos de testes serão executados
+#define TESTE_NUMERO 7 //Há três testes, que varia o número de Threads e de Casos de Teste para apurar as respostas [teste: threads x casos de teste]
+						// 1: 4  x  50
+						// 2: 4  x 100
+						// 3: 8  x 100
+						// 4: 8  x 150
+						// 5: 16 x 150
+						// 6: 16 x 200
+						// ?: 32 x 200
+
+#if TESTE_NUMERO == 1
+	#define NUM_THREADS 4 //Define número de threads
+	#define CASOSTESTES 50 //Define a quantidade de casos de testes serão executados
+#elif TESTE_NUMERO == 2
+	#define NUM_THREADS 4 //Define número de threads
+	#define CASOSTESTES 100 //Define a quantidade de casos de testes serão executados
+#elif TESTE_NUMERO == 3
+	#define NUM_THREADS 8 //Define número de threads
+	#define CASOSTESTES 100 //Define a quantidade de casos de testes serão executados
+#elif TESTE_NUMERO == 4
+	#define NUM_THREADS 8 //Define número de threads
+	#define CASOSTESTES 150 //Define a quantidade de casos de testes serão executados
+#elif TESTE_NUMERO == 5
+	#define NUM_THREADS 16 //Define número de threads
+	#define CASOSTESTES 150 //Define a quantidade de casos de testes serão executados
+#elif TESTE_NUMERO == 6
+	#define NUM_THREADS 16 //Define número de threads
+	#define CASOSTESTES 200 //Define a quantidade de casos de testes serão executados
+#else
+	#define NUM_THREADS 32 //Define número de threads
+	#define CASOSTESTES 200 //Define a quantidade de casos de testes serão executados
+#endif // TESTE_NUMERO == 1
+
 
 int GLOBALVAR = 0; //variável compartilhada
+
 
 void test_sum(){
 	DESCRIBE("SUM");
@@ -37,17 +78,8 @@ void test_sub() {
 	isEqual(sum(0, 0), 0);
 }
 
-char* concat(char *txt, int num) {
-	char buffer[255];
-	sprintf(buffer, txt, num);
-	return buffer;
-}
-
 void *plusOne(void *arg) {
 	//Função paralela que incrementa um valor na variável global
-
-	DESCRIBE("SOMA GLOBAL");
-	WHEN("I execute sum");
 
 	int valGlobal = GLOBALVAR;
 	int result = valGlobal + 1;
@@ -56,11 +88,13 @@ void *plusOne(void *arg) {
 	char thentxt[50];
 	sprintf(iftxt, "use numbers %ld and 1", GLOBALVAR);
 	sprintf(thentxt, "the result should be %ld", result);
-	
-	IF(iftxt);
-	THEN(thentxt);
 
-	isEqual(sum(GLOBALVAR, 1), result); //Verifica se a soma surtiu efeito 
+//	DESCRIBE("SOMA THREAD");
+//	WHEN("I execute sum using threads");
+//	IF(iftxt);
+//	THEN(thentxt);
+//	isEqual(sum(GLOBALVAR, 1), result); //Verifica se a soma surtiu efeito 
+
 	GLOBALVAR = result; //Atribui a variavel compartilhada o valor da soma no inicio do processo
 }
 
@@ -80,6 +114,7 @@ void doTheParallel() {
 
 void contaCasos() {
 	//Função que conta a quantidade de possibilidades de resultados
+	
 	int contador[NUM_THREADS + 1]; //Teoricamente, há NUM_THREADS - 1 casos de resposta, de 1 à NUM_THREAD
 									//No pior caso, cada thread é executada uma após a outra, resultando em NUM_THREAD
 	for (int i = 0; i < NUM_THREADS + 1; i++) {
@@ -102,7 +137,7 @@ void contaCasos() {
 
 	char iftxt[255];
 	char thentxt[255];
-	sprintf(iftxt, "used %ld cases of test", CASOSTESTES);
+	sprintf(iftxt, "used %ld cases of test and %ld threads", CASOSTESTES, NUM_THREADS);
 	sprintf(thentxt, "it was possible obtained a tottal of %ld different answer", totalRespostasDiferentes);
 
 	DESCRIBE("COUNTER");
@@ -112,6 +147,7 @@ void contaCasos() {
 }
 
 int main () {
+	
 	//test_sum();
 	//test_sub();
 
@@ -121,7 +157,6 @@ int main () {
 	//4. Criar casos de teste para a função usando simpletest. 
 	//5. Submeter um único arquivo de texto contendo o código com a função e os casos de teste.
 
-	contaCasos();
-
+	contaCasos(); // Inicia o Teste, conforme as configurações feitas no #define TESTE_NUMERO
 	system("PAUSE"); //Permite que se veja a saida dos dados
 }
